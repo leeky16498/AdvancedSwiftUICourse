e80e6ad14b4fd4aa103aeb350fc1a9385ec9e32c
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-06 11:31:04 +0000

    Initial Commit

---
+++ AdvancedSwiftUICourse/AdvancedSwiftUICourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUICourseApp.swift
+//  AdvancedSwiftUICourse
+//
+//  Created by Kyungyun Lee on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUICourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
